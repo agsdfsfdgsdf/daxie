e8275dd98f1fc84817100d07786845b4eda7e171
agsdfsfdgsdf <m17710695626@163.com>
2023-11-27 12:26:46 +0800

    kafka发送消息

--- src/main/java/com/deyi/daxie/cloud/service/config/HttpConfig.java
+++
@@ -1,6 +0,0 @@
-package com.deyi.daxie.cloud.service.config;
-
-public class HttpConfig {
-    // public static final String TCS_BASE_URL = "10.2.180.196:7130"; // 测试环境
-    public static final String TCS_BASE_URL = "10.7.3.171:7135"; // 生产环境
-}
--- src/main/java/com/deyi/daxie/cloud/service/listener/LessonMsgService.java
+++
@@ -1,7 +0,0 @@
-package com.deyi.daxie.cloud.service.listener;
-
-import com.alibaba.fastjson.JSONObject;
-
-public interface LessonMsgService {
-    void save(String message);
-}
